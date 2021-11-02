5c7d72c05105d90ffdc8d39e98d9cc48d3773fb1
JOE MON <56827756+JOEMN96@users.noreply.github.com>
2021-11-02 08:03:02 +0530

    Morning Study

--- advancedStorage/public/app.js
+++ advancedStorage/public/app.js
@@ -1,19 +1,28 @@
 const ABI = [
   {
     constant: true,
-    inputs: [
+    inputs: [],
+    name: 'last_completed_migration',
+    outputs: [
       {
         internalType: 'uint256',
         name: '',
         type: 'uint256',
       },
     ],
-    name: 'ids',
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'owner',
     outputs: [
       {
-        internalType: 'uint256',
+        internalType: 'address',
         name: '',
-        type: 'uint256',
+        type: 'address',
       },
     ],
     payable: false,
@@ -25,73 +34,46 @@ const ABI = [
     inputs: [
       {
         internalType: 'uint256',
-        name: '_id',
+        name: 'completed',
         type: 'uint256',
       },
     ],
-    name: 'add',
+    name: 'setCompleted',
     outputs: [],
     payable: false,
     stateMutability: 'nonpayable',
     type: 'function',
   },
-  {
-    constant: true,
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: '_index',
-        type: 'uint256',
-      },
-    ],
-    name: 'get',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-    ],
-    payable: false,
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: 'getAll',
-    outputs: [
-      {
-        internalType: 'uint256[]',
-        name: '',
-        type: 'uint256[]',
-      },
-    ],
-    payable: false,
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: 'getLength',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-    ],
-    payable: false,
-    stateMutability: 'view',
-    type: 'function',
-  },
 ];
 
-const web3 = new Web3(Web3.currntProvider || 'ws://localhost:9545');
+// const addresss = '0xF19Aef1e7D3425B81d3Bf7D7b6720C6795DFF165';
 
-const addresss = '';
+// let strorageContract = new web3.eth.Contract(ABI, addresss);
 
-let strorageContract = new web3.eth.Contract(ABI, addresss);
+async function inititalizeApp() {
+  if (typeof window.ethereum !== 'undefined') {
+    try {
+      const USER = await ethereum.request({ method: 'eth_requestAccounts' });
+      document.querySelector('body').innerHTML = '<h2> Req Accepted </h2>';
+      return new Promise((resolve, reject) => {
+        resolve(USER);
+      });
+    } catch (e) {
+      document.querySelector('body').innerHTML = '<h2> Req Rejected </h2>';
+      return new Promise((resolve, reject) => {
+        let web3 = new Web3('http://127.0.0.1:9545');
+        if (web3) {
+          resolve(web3);
+        } else {
+          reject({ err: 'Something Went Wrong' });
+        }
+      });
+    }
+  }
+}
 
-document.addEventListener('DOMContentLoaded', async () => {});
+document.addEventListener('DOMContentLoaded', async () => {
+  let USER = await inititalizeApp();
+  console.log('from local');
+  console.log(USER);
+});
